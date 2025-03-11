482cab8ea141990703f6de6ae16c7174753d5999
SUYASH-5 <122517951+SUYASH-5@users.noreply.github.com>
2025-03-11 11:50:41 +0530

    Add files via upload

---
+++ test/main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int largest = arr[0];
+        int secondlargest = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > largest) {
+                secondlargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secondlargest && arr[i] != largest) {
+                secondlargest = arr[i];
+            }
+        }
+        System.out.println("second largest number is: " + secondlargest);
+    }
+}
